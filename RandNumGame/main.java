e50a7a3a5d86e0472feffe29e49526fc91ec9a39
Engineering <Engineering@MacBook-Air.cls-edu.charlottelatin.org>
2022-09-01 13:06:43 -0400

    stuff

--- RandNumGame/main.java
+++ RandNumGame/main.java
@@ -4,7 +4,7 @@ public class main {
     
     static privateClassExample pClassE = new privateClassExample();
 
-    public static void main(String args[]){
+    public static void main(String[] args) {
         //main.oneRangeGame();
         main.dynamicRangeGame();
     }
