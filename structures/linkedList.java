0ef1ac822c0ff1c968caf544ecc11516ac29bfcb
Engineering <Engineering@MacBook-Air.cls-edu.charlottelatin.org>
2022-09-01 13:39:54 -0400

    updated structures

--- structures/linkedList.java
+++ structures/linkedList.java
@@ -2,14 +2,16 @@ public class linkedList {
     public static void main(String[] args) {
     linkedList lList = new linkedList();
     lList.add(5);
-    //lList.add(7);
+    lList.add(7);
+    lList.add(6);
+    lList.remove(7);
     lList.printItems();
     }
 
     node head;
 
     linkedList(){
-        head = new node();
+        head = new node(null);
     }
     
     void printItems(){
@@ -21,8 +23,33 @@ public class linkedList {
     }
 
     void add(Object data){
-        if(head.data == null){
-            head.data = data;
+        node current = head;
+        while(current.next != null){
+            current = current.next;
         }
+        if(current.data == null){
+            current.data = data;
+        }else{
+            current.next = new node(data);
+        }
+    }
+
+    void remove(Object data){
+        node current = head;
+        node last = head;
+        while(current != null){
+            if (current.data == data){
+                if(current == head){
+                    head = head.next;
+                }else{
+                    last.next = current.next;
+                }
+                System.out.println("found and removed the first instance of " + data);
+                return;
+            }
+            last = current;
+            current = current.next;
+        }
+        System.out.println("could not remove " + data);
     }
 }
--- structures/node.java
+++ structures/node.java
@@ -1,4 +1,8 @@
 public class node {
     Object data;
     node next;
+
+    node (Object d){
+        data = d;
+    }
 }
