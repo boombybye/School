9477d203bb7bd3b3e13d481d8babdb51a20b3bd0
Engineering <Engineering@MacBook-Air.cls-edu.charlottelatin.org>
2022-09-14 13:46:06 -0400

    added variable to card class

--- structures/playingCard.java
+++ structures/playingCard.java
@@ -1,11 +1,17 @@
 public class playingCard {
     
+    int value;
     char type;
     char suit;
+    boolean color;
+    boolean faceUp;
 
-    playingCard(char t, char s){
+    playingCard(int v, char t, char s, boolean c, boolean f){
+        value = v;
         type = t;
         suit = s;
+        color = c;
+        faceUp = f;
     }
 
 
